7c1bd651173db9e70df6f0117201531145b9de30
ekfgo0 <anstjsdud0306@gmail.com>
2025-07-17 21:08:27 +0900

    style: 중복확인, 전송 버튼이 선택창 우측에 오도록 수정

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -23,121 +23,146 @@ const Signup = () => {
         {/* 국적 선택 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">국적</label>
+        </div>
+
+        <div>
           <select
             className="border px-3 py-2 rounded text-gray-700"
-            style={{ width: "508px", height: "40px" }}
+            style={{ width: "310px", height: "40px" }}
           >
             <option>선택하세요</option>
           </select>
         </div>
+
 
         {/* 소속대학 선택 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">소속대학</label>
+        </div>
+
+        <div>
           <select
             className="border px-3 py-2 rounded text-gray-700"
-            style={{ width: "508px", height: "40px" }}
+            style={{ width: "310px", height: "40px" }}
           >
             <option>선택하세요</option>
           </select>
         </div>
 
+
         {/* 닉네임 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">닉네임</label>
-          <div className="flex justify-between">
-            <input
-              type="text"
-              className="border px-3 py-2 rounded"
-              style={{ width: "500px", height: "35px"}}
-            />
-            <button
-              className="border px-3 py-2 rounded text-blue-500 border-blue-400"
-              style={{ width: "100px" }}
-            >
-              중복 확인
-            </button>
-          </div>
         </div>
+
+        <div className="flex justify-between">
+          <input
+            type="text"
+            className="border px-3 py-2 rounded"
+            style={{ width: "300px", height: "35px"}}
+          />
+          <button
+            className="border px-3 py-2 rounded text-blue-500 border-blue-400"
+            style={{ width: "100px" }}
+          >
+            중복 확인
+          </button>
+        </div>
+
 
         {/* 아이디 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">아이디</label>
-          <div className="flex justify-between">
-            <input
-              type="text"
-              className="border px-3 py-2 rounded"
-              style={{ width: "500px", height: "35px"}}
-            />
-            <button
-              className="border px-3 py-2 rounded text-blue-500 border-blue-400"
-              style={{ width: "100px" }}
-            >
-              중복 확인
-            </button>
-          </div>
         </div>
+
+        <div className="flex justify-between">
+          <input
+            type="text"
+            className="border px-3 py-2 rounded"
+            style={{ width: "300px", height: "35px"}}
+          />
+          <button
+            className="border px-3 py-2 rounded text-blue-500 border-blue-400"
+            style={{ width: "100px" }}
+          >
+            중복 확인
+          </button>
+        </div>
+
 
         {/* 비밀번호 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">비밀번호</label>
+        </div>
+
+        <div>
           <input
             type="password"
             className="border px-3 py-2 rounded"
-            style={{ width: "500px", height: "35px" }}
+            style={{ width: "300px", height: "35px" }}
           />
         </div>
+
 
         {/* 비밀번호 확인 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">비밀번호 확인</label>
+        </div>
+
+        <div>
           <input
             type="password"
             className="border px-3 py-2 rounded"
-            style={{ width: "500px", height: "35px" }}
+            style={{ width: "300px", height: "35px" }}
           />
         </div>
 
+
         {/* 본인 대학 이메일 */}
         <div>
-          <label className="block mb-1 text-sm font-medium text-gray-700">본인 대학 이메일</label>
-          <div className="flex gap-2" style={{ width: "100%" }}>
-            <input
-              type="email"
-              className="border px-3 py-2 rounded"
-              style={{ width: "500px", height: "35px" }}
-            />
-            <button
-              className="border px-3 py-2 rounded text-blue-500 border-blue-400"
-              style={{ width: "100px" }}
-            >
-              전송
-            </button>
-          </div>
+          <label className="block mb-1 text-sm font-medium text-gray-700">본인대학 이메일</label>
         </div>
+
+        <div className="flex gap-2" style={{ width: "100%" }}>
+          <input
+            type="email"
+            className="border px-3 py-2 rounded"
+            style={{ width: "300px", height: "35px" }}
+          />
+          <button
+            className="border px-3 py-2 rounded text-blue-500 border-blue-400"
+            style={{ width: "100px" }}
+          >
+            전송
+          </button>
+        </div>
+
 
         {/* 인증번호 */}
         <div>
           <label className="block mb-1 text-sm font-medium text-gray-700">인증번호</label>
-          <div className="flex gap-2" style={{ width: "100%" }}>
-            <input
-              type="text"
-              className="border px-3 py-2 rounded"
-              style={{ width: "500px", height: "35px" }}
-            />
-            <button
-              className="border px-3 py-2 rounded text-blue-500 border-blue-400"
-              style={{ width: "100px" }}
-            >
-              전송
-            </button>
-          </div>
         </div>
+
+
+        <div className="flex gap-2" style={{ width: "100%" }}>
+          <input
+            type="text"
+            className="border px-3 py-2 rounded"
+            style={{ width: "300px", height: "35px" }}
+          />
+          <button
+            className="border px-3 py-2 rounded text-blue-500 border-blue-400"
+            style={{ width: "100px" }}
+          >
+              전송
+          </button>
+        </div>
+
 
         {/* 가입 버튼 */}
         <button
           className="bg-blue-500 text-white py-2 rounded mt-2"
-          style={{ width: "508px", height: "35px" }}
+          style={{ width: "310px", height: "35px" }}
         >
           회원가입
         </button>
